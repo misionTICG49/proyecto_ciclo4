eadccc553857c3568555c9e5c648a8ed11438046
fsmolinas <fsmolinas@gmail.com>
2022-11-26 21:07:31 -0500

    update database to mogoatlas

--- config/database.js
+++ config/database.js
@@ -1,12 +1,17 @@
+/*import Pass from "./Pass.json";
+
+const { pass } = Pass;*/
+
 const mongoose = require ("mongoose");
 
-const host = "localhost";
+/*const host = "localhost";
 const port = "27017";
-const db = "herencia";
+const db = "herencia";*/
 
 exports.mongoConnect = () =>{
-    const mongoStringConnection = `mongodb://${host}:${port}/${db}`;
-    
+    const mongoStringConnection = `mongodb+srv://Master:admin@cluster0-g49.izjz7gk.mongodb.net/herencia` 
+    //const mongoStringConnection = `mongodb+srv://Master:admin@cluster0-g49.izjz7gk.mongodb.net/herencia` 
+    // const mongoStringConnection = `mongodb://${host}:${port}/${db}`; 
     mongoose.connect(mongoStringConnection);
     mongoose.Promise = global.Promise;
     const dbConnection = mongoose.connection;
